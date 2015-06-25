45f4ef76332c52d3ad62031b133d614b8b840df7
Ingrid <ingrid.mazoni01@fatec.sp.gov.br>
2015-06-25 12:19:29 -0300

    Adicionado projeto livraria-site

---
+++ livraria-site/src/main/java/br/com/caelum/vraptor/controller/MundoController.java
@@ -0,0 +1,10 @@
+package br.com.caelum.vraptor.controller;
+
+import br.com.caelum.vraptor.Resource;
+
+@Resource
+public class MundoController {
+	public void ola() {
+		System.out.println("Olá Mundo!");
+	}
+}
