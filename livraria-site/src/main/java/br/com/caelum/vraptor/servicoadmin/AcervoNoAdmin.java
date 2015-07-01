daceaabe55f5f586246233bb3215f286ed850c0b
Ingrid <ingrid.mazoni01@fatec.sp.gov.br>
2015-07-01 13:24:46 -0300

    adicionada anotation

--- livraria-site/src/main/java/br/com/caelum/vraptor/servicoadmin/AcervoNoAdmin.java
+++ livraria-site/src/main/java/br/com/caelum/vraptor/servicoadmin/AcervoNoAdmin.java
@@ -3,8 +3,10 @@ package br.com.caelum.vraptor.servicoadmin;
 import java.util.List;
 
 import br.com.caelum.vraptor.Entity.Livro;
+import br.com.caelum.vraptor.ioc.Component;
 import br.com.caelum.vraptor.repository.Acervo;
 
+@Component
 public class AcervoNoAdmin implements Acervo {
 
 	public List<Livro> todosOsLivros() {
