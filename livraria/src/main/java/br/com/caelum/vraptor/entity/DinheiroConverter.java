fa3493de2fc51078da5bce9fe335ff58a62ea402
Ingrid <ingrid.mazoni01@fatec.sp.gov.br>
2015-07-29 14:07:00 -0300

    alterações no converter

--- livraria/src/main/java/br/com/caelum/vraptor/entity/DinheiroConverter.java
+++ livraria/src/main/java/br/com/caelum/vraptor/entity/DinheiroConverter.java
@@ -11,7 +11,7 @@ public class DinheiroConverter implements Converter<Dinheiro> {
 
 	public Dinheiro convert(String value, Class<? extends Dinheiro> type,ResourceBundle bundle) {
 		
-		return new Dinheiro(Moeda.DOLAR, new BigDecimal(value));
+		return new Dinheiro(Moeda.REAL, new BigDecimal(value));
 	}
 
 }
